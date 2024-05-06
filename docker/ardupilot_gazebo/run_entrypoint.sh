bc52ade10c91a06439aa408769e795731e0ca861
czuczo <picmego@gmail.com>
2024-05-06 15:13:08 +0200

    gazebo+ardupilo_sitl works

--- docker/ardupilot_gazebo/run_entrypoint.sh
+++ docker/ardupilot_gazebo/run_entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Source the setup.bash script
-source /workspace/install/setup.bash
+echo 'source /workspace/install/setup.bash' >> ~/.bashrc
 
-# Start a bash shell
-exec /bin/bash
-
-# and add this at the end
-exec "$@"
+# configure gazebo
+echo 'export GZ_SIM_SYSTEM_PLUGIN_PATH=/workspace/ardupilot_gazebo_ws/src/ardupilot_gazebo/build:${GZ_SIM_SYSTEM_PLUGIN_PATH}' >> ~/.bashrc
+echo 'export GZ_SIM_RESOURCE_PATH=/workspace/ardupilot_gazebo_ws/src/ardupilot_gazebo/models:/workspace/ardupilot_gazebo_ws/src/ardupilot_gazebo/worlds:${GZ_SIM_RESOURCE_PATH}' >> ~/.bashrc
+# configure ardupilot sitl
+echo 'export PATH=$PATH:/workspace/ardupilot_sitl/src/ardupilot/Tools/autotest' >> ~/.bashrc 
+echo 'export PATH=/usr/lib/ccache:$PATH' >> ~/.bashrc 
